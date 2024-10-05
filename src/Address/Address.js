b3b260e96aea30205298697179e18a87a6da2b76
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-10-05 15:44:57 +0530

    inital commit

--- src/Address/Address.js
+++ src/Address/Address.js
@@ -1,34 +1,31 @@
 "use client";
-// import Address from "@/Address/Address";
 import axios from "axios";
 import React, { useState } from "react";
+import "../app/globals.css"
 
-export default function Home() {
-  const [data, setData] = useState(null);
+const Address = () => {
+  const [data, setData] = useState(null); 
 
   const location = async () => {
     try {
       const ipResponse = await axios.get("https://ipapi.co/json/");
       const ipData = ipResponse.data;
-      setData(ipData);
+      setData(ipData); 
     } catch (error) {
       console.error("Error fetching IP data:", error);
     }
   };
 
+  console.log(data)
+
   return (
     <>
-      <div className="mt-5 flex justify-evenly items-center ">
-        <h1 className="text-[16px]  " > Get Your Details from IP </h1>
-        <button
-          onClick={location}
-          className="bg-violet-500 px-4 py-3 border text-white rounded-lg "
-        >
-          Get IP Detail
-        </button>
-      </div>
+     <div>
+     <button onClick={location} className="bg-violet-500" >Get IP Detail</button>
+     </div>
       {data && (
-        <div className="text-center mt-8 ">
+        <div className="text-center" >
+        
           <h2>IP Details:</h2>
           <ul>
             {Object.entries(data).map(([key, value], index) => (
@@ -41,4 +38,6 @@ export default function Home() {
       )}
     </>
   );
-}
+};
+
+export default Address;
